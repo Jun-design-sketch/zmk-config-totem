ee2f5e4637127a447ae48ea3f73f0c171184aefc
Jun <83737699+Jun-design-sketch@users.noreply.github.com>
2026-03-28 00:42:18 +0900

    Update default scroll value in pointing config

--- config/pointing_config.h
+++ config/pointing_config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define ZMK_POINTING_DEFAULT_MOVE_VAL 600  // default 600
-#define ZMK_POINTING_DEFAULT_SCRL_VAL 20    // default 10
+#define ZMK_POINTING_DEFAULT_MOVE_VAL 600  // default
+#define ZMK_POINTING_DEFAULT_SCRL_VAL 10    // default
 
 #include <dt-bindings/zmk/pointing.h>
